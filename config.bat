6e70da18f503ed7597fa5f894943b1ead375e888
alanserafim <alan.serafim@fatec.sp.gov.br>
2022-05-18 18:30:48 -0300

    Login funcionando e arquivos bat

--- config.bat
+++
@@ -1,7 +0,0 @@
-npm install express --save
-npm install nodemon -g
-npm install sha1
-npm install mysql2
-npm install body-parser
-npm install ejs
-npm install consign
--- controller/routes/usuario.js
+++ controller/routes/usuario.js
@@ -1,5 +1,5 @@
 const seguranca = require('../../model/components/seguranca')
-const usuarioBanco = require('../../controller/SQL/usuarioDB')
+const usuarioBanco = require('../../model/repositories/usuarioDB')
 
 module.exports = function (app){
 
@@ -40,7 +40,8 @@ module.exports = function (app){
     })
 
     app.get('/login', function (req, res){
-        res.render('pages/form-1login');
+        if(req.query.fail) res.render('pages/form-1login', { mensagemLogin: 'Usúario e/ou senha incorretos!'});
+        else res.render('pages/form-1login', { mensagemLogin: null});
     })
 
     app.get('/cadastro', function (req, res){
@@ -56,3 +57,92 @@ module.exports = function (app){
     });
 
 }
+
+
+
+/*
+module.exports = function (app){
+
+    //GET da Página Inicial
+    app.get("/", function(req, resp){
+        resp.send("<h1>Bem-vindo ao meu app</h1>");
+    })
+
+    //GET da Página de cadastro de novos usuários    
+    app.get('/cadastro', function (req, res){
+        if(req.query.fail) 
+            res.render('usuario/CadastroUsuario', {mensagem: 'Cadastro'});
+        else
+            res.render('usuario/CadastroUsuario', {mensagem: null});
+
+    })
+    
+    //POST da página EditUsuario.ejs
+    app.post('/cadastro/usuario/edit/salvar', (req, res) => {
+        var usuario = { 
+            nome: req.body.nome,
+            senha: req.body.senha,
+            id: req.body.id
+        };
+        try {
+            usuarioBanco.updateUsuario(usuario);
+            res.render('usuario/Sucesso', {mensagem: 'alterado'});
+        } catch (error){
+            res.render('usuario/EditUsuario', {title: 'Edição Cadastro', mensagem: "Erro no cadastro"})
+        }
+    })
+
+    //POST da página CadastroUsuario.ejs 
+    app.post('/cadastro/usuario/salvar', seguranca.autenticar, (req, res) => {
+        try {
+            var usuario = {nome: req.body.nome,
+                           senha: seguranca.ocultarsenha(req.body.senha)}
+            usuarioBanco.insertUsuario(usuario);
+            res.render('usuario/Sucesso', {mensagem: 'cadastrado'});
+        } catch (error){
+            res.render('usuario/CadastroUsuario', { title: 'Cadastro', mensagem: "Erro no cadastro"})
+        }
+    })
+
+    //GET da página lista.ejs
+    app.get('/lista/usuario', seguranca.autenticar, async (req, res, next) => {
+        try{
+            const docs = await usuarioBanco.selectUsuario();
+            res.render('usuario/Lista', { mensagem: 'Lista de Usuário', docs });
+        } catch (err){
+            next(err);
+        }
+    });
+
+    //GET do botão delete da página lista.ejs
+    app.get('/delete/usuario/:id', seguranca.autenticar, async (req, res, next) => {
+        try{
+            var id = req.params.id;
+            await usuarioBanco.deleteUsuario(id);
+            const docs = await usuarioBanco.selectUsuario();
+            res.render('usuario/Lista', { mensagem: 'Usuário excluído com sucesso', docs });
+        } catch (err){
+            next(err);
+        }
+    });
+
+    //GET do botão editar da página lista.ejs
+    app.get('/edit/usuario/:id', seguranca.autenticar, async (req, res, next) => {
+        try{
+            var id = req.params.id;
+            const usuario = await usuarioBanco.getUsuarioId(id);
+            res.render('usuario/EditUsuario', { mensagem: '', usuario });
+        } catch (err){
+            next(err);
+        }
+    });
+
+    //GET da página Login.ejs
+    app.get('/login', function (req, res) {
+        if(req.query.fail) res.render('usuario/Login', { mensagemLogin: 'Usúario e/ou senha incorretos!'});
+        else res.render('usuario/Login', { mensagemLogin: null});
+    });
+
+}
+
+*/
