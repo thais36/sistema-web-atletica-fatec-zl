18795e139a4fcb1c29af01b24693d70ccb699978
Thais Almeida <90707459+thais36@users.noreply.github.com>
2022-05-31 16:10:44 -0300

    Update modalidades.js

--- controller/routes/modalidades.js
+++ controller/routes/modalidades.js
@@ -3,8 +3,25 @@ const usuarioBanco = require('../../model/repositories/usuarioDB')
 
 module.exports = function (app){
     
-    app.get('/modalidades/inscricao', function (req, res){
+    app.get('/modalidades-inscricao', function (req, res){
         res.render('forms/form-4inscricaoModalidades');
     });
+
+        //POST da página EditInscricaoModalidades.ejs
+        app.post('/cadastro/usuario/edit/salvar', (req, res) => {
+            var modalidades = {
+                nome: req.body.nome,
+                genero: req.body.genero,
+                tipo: req.body.tipo,
+                horario: req.body.tipo,
+                dia_da_semana: req.body.dia_da_semana,
+                equipamento: req.body.equipamento
+            }
+            usuarioBanco.insertInscricaoModalidade(modalidades);
+            res.render('pages/sucesso-cadastro', {mensagem: 'inscrito'});
+        } catch (error){
+            res.render('pages/form-4inscricaomodalidades', { title: 'Inscrição-Modalidades', mensagem: "Erro na inscrição"})
+        }
+        
      
 }
